4232015457184910b611d8fbacba3a3219ed97b6
ashutosh2928 <76245890+ashutosh2928@users.noreply.github.com>
2023-12-19 05:03:15 +0000

    commit changes

---
+++ basics/variable_dec.js
@@ -0,0 +1,8 @@
+const a1 = 2
+//a1 = 3 // error bcoz its const cant be changed
+console.log(a1)
+
+// for variable values
+let a = 3
+a = 5 // here it works... same with var .. but it has loop holes that it changes value within scope 
+console.log(a)
